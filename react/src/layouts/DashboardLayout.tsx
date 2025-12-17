4afb01d5dc7ee50ad4589220425d58a8bf661532
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-12-17 16:36:48 -0300

    feat: added DashboardLayout

---
+++ react/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../pages/Dashboard/components/Navbar";
+import SideBar from "../pages/Dashboard/components/SideBar";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Navbar />
+      <div className="flex flex-1">
+        <SideBar />
+        <main className="flex-1 bg-primary-200 p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
