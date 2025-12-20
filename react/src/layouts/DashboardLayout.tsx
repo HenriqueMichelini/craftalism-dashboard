c3e4b850fa26f2fa9906ceeaf02b2b5b6131eabc
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-12-19 22:39:30 -0300

    feat: improved SideBar with providing future state

--- react/src/layouts/DashboardLayout.tsx
+++ react/src/layouts/DashboardLayout.tsx
@@ -1,14 +1,12 @@
 import Navbar from "../pages/Dashboard/components/Navbar";
 import SideBar from "../pages/Dashboard/components/SideBar";
-import SideBarOpened from "../pages/Dashboard/components/SideBarOpened";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
       <Navbar />
       <div className="flex flex-1">
-        <SideBar />
-        <SideBarOpened />
+        <SideBar isOpen={false} />
         <main className="flex-1 bg-primary-200 p-6">{children}</main>
       </div>
     </div>
--- react/src/pages/Dashboard/components/SideBar.tsx
+++ react/src/pages/Dashboard/components/SideBar.tsx
@@ -1,34 +1,46 @@
 import { AlignEndVertical, Box, Database } from "lucide-react";
 import type { LucideIcon } from "lucide-react";
 
+type SideBarState = {
+  isOpen: boolean;
+};
+
 type SideBarItemProps = {
   icon: LucideIcon;
   label: string;
 };
 
-function SideBar() {
+function SideBar({ isOpen }: SideBarState) {
   return (
-    <nav className="flex h-full w-20 flex-col items-center gap-6 bg-gradient-to-br from-primary-500 to-primary-400 py-6">
-      <SideBarItem icon={Database} label="Database" />
-      <SideBarItem icon={AlignEndVertical} label="Analytics" />
-      <SideBarItem icon={Box} label="Products" />
+    <nav
+      className={`flex h-full flex-col items-center gap-6 bg-gradient-to-br from-primary-500 to-primary-400 py-6 transition-all ${isOpen ? "w-64" : "w-20"}`}
+    >
+      <SideBarItem icon={Database} label="Database" isOpen={isOpen} />
+      <SideBarItem icon={AlignEndVertical} label="Analytics" isOpen={isOpen} />
+      <SideBarItem icon={Box} label="Products" isOpen={isOpen} />
     </nav>
   );
 }
 
-function SideBarItem({ icon: Icon, label }: SideBarItemProps) {
+function SideBarItem({
+  icon: Icon,
+  label,
+  isOpen,
+}: SideBarItemProps & { isOpen: boolean }) {
   return (
     <button
       title={label}
-      className="
-      flex size-10
-      items-center justify-center
-      rounded-md
-      text-default
-      hover:bg-primary-400 shadow-md hover:shadow-lg
-      "
+      className={`
+        flex items-center rounded-md text-default
+        hover:bg-primary-400
+        ${isOpen ? "w-full gap-3 px-6 py-2" : "size-10 justify-center"}
+      `}
     >
-      <Icon className="size-6" />
+      <Icon className="size-6 shrink-0" />
+
+      {isOpen && (
+        <span className="whitespace-nowrap text-sm font-medium">{label}</span>
+      )}
     </button>
   );
 }
