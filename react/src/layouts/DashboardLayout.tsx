100c44bff8fc2e20fc5d1200d67d443fe11ea5a8
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-12-30 02:31:15 -0300

    feat: added state change to sidebar

--- react/src/layouts/DashboardLayout.tsx
+++ react/src/layouts/DashboardLayout.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Navbar from "../pages/Dashboard/components/Navbar";
 import SideBar from "../pages/Dashboard/components/SideBar";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex h-screen flex-col">
-      <Navbar />
+      <Navbar onToggleSidebar={() => setIsSidebarOpen((prev) => !prev)} />
+
       <div className="flex flex-1">
-        <SideBar isOpen={false} />
+        <SideBar isOpen={isSidebarOpen} />
         <main className="flex-1 space-y-4 bg-primary-200 p-6">{children}</main>
       </div>
     </div>
--- react/src/pages/Dashboard/components/Navbar.tsx
+++ react/src/pages/Dashboard/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import { Menu } from "lucide-react";
 
-function Navbar() {
+type NavbarProps = {
+  onToggleSidebar: () => void;
+};
+
+function Navbar({ onToggleSidebar }: NavbarProps) {
   return (
     <nav className="flex h-16 items-center bg-gradient-to-tr from-primary-500 to-primary-400">
       <div className="flex items-center">
         <div className="flex h-full w-20 items-center justify-center">
-          {SideBarButton()}
+          <SideBarButton onClick={onToggleSidebar} />
         </div>
+
         <span className="text-navbar font-semibold text-default">
           Craftalism Dashboard
         </span>
@@ -15,9 +20,12 @@ function Navbar() {
   );
 }
 
-function SideBarButton() {
+function SideBarButton({ onClick }: { onClick: () => void }) {
   return (
-    <button>
+    <button
+      onClick={onClick}
+      className="flex size-10 items-center justify-center rounded-md hover:bg-primary-300"
+    >
       <Menu className="size-6 text-default" />
     </button>
   );
